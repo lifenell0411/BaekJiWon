5f13969f452fcbf8e26cf8f90fd0c70d654d5c39
lifenell0411 <lifenell0411@gmail.com>
2023-04-30 23:44:51 +0900

    실행환경세팅, 프로젝트시작

---
+++ src/test/java/com/KoreaIT/bjw/_05_project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KoreaIT.bjw._05_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
