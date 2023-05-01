fee30b83741f6f11910338fada4ab522843883f5
lifenell0411 <lifenell0411@gmail.com>
2023-05-01 23:00:20 +0900

    ArticleService 및 ArticleRepository 도입

--- src/main/java/com/KoreaIT/bjw/_05_project/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/bjw/_05_project/controller/UsrHomeController.java
@@ -1,49 +1,17 @@
 package com.KoreaIT.bjw._05_project.controller;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 @Controller
 public class UsrHomeController {
 
-	@RequestMapping("/usr/home/getArticles")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1");
-		Article article2 = new Article(2, "제목2");
+	public String showMain() {
 
-		List<Article> articles = new ArrayList<>();
-		articles.add(article1);
-		articles.add(article2);
-
-		return articles;
+		return "MAIN HI";
 	}
-
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1");
-
-		return article;
-	}
-
-	 
- 
-}
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Article {
-	private int id;
-	private String title;
 
 }
---
+++ src/main/java/com/KoreaIT/bjw/_05_project/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.KoreaIT.bjw._05_project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.KoreaIT.bjw._05_project.repository.ArticleRepository;
+import com.KoreaIT.bjw._05_project.vo.Article;
+
+@Service
+public class ArticleService {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
+	}
+
+	// 서비스 메서드
+	public Article writeArticle(String title, String body) {
+		return articleRepository.writeArticle(title, body);
+	}
+
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+
+	public List<Article> articles() {
+		return articleRepository.getArticles();
+	}
+}
