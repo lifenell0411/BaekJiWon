511f25a47419ed08f3161f42dad143a5c3863698
lifenell0411 <lifenell0411@gmail.com>
2023-05-01 22:50:31 +0900

    ArticleController추가, 게시글작성 및 리스트구현, 게시글테스트데이터 생성

---
+++ src/main/java/com/KoreaIT/bjw/_05_project/controller/UsrArticleController.java
@@ -0,0 +1,53 @@
+package com.KoreaIT.bjw._05_project.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.KoreaIT.bjw._05_project.vo.Article;
+@Controller
+public class UsrArticleController {
+	int lastArticleId;
+	List<Article> articles;
+
+	public UsrArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		lastArticleId++;
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+}
