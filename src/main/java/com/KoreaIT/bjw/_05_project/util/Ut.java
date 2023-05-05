8ca24e517829b4403fa04a18c2782f771e5623be
lifenell0411 <lifenell0411@gmail.com>
2023-05-05 23:05:31 +0900

    Ut.empty함수 도입, 중복제거

---
+++ src/main/java/com/KoreaIT/bjw/_05_project/util/Ut.java
@@ -0,0 +1,21 @@
+package com.KoreaIT.bjw._05_project.util;
+
+
+public class Ut {
+
+	public static boolean empty(Object obj) {
+
+		if (obj == null) {
+			return true;
+		}
+
+		if (obj instanceof String == false) {
+			return true;
+		}
+
+		String str = (String) obj;
+
+		return str.trim().length() == 0;
+	}
+
+}
